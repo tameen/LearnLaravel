5712fbacfa1a58e613ea2c90dbe10d8e4a56c46b
Atif Saeed <atifsaeed14@gmail.com>
2023-09-10 04:26:13 +0500

    artisan make:middleware Role

---
+++ app/Http/Middleware/Role.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Role
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, $role): Response
+    {
+        if ($request->user()->role !== $role){
+            if ($request->user()->role === 'admin') {
+                return redirect('admin/dashboard');
+            } elseif ($request->user()->role === 'agent') {
+                return redirect('agent/dashboard');
+            } elseif ($request->user()->role === 'user') {
+                return redirect('dashboard');
+            }
+            return redirect('dashboard');
+        } 
+        return $next($request);
+    }
+}
