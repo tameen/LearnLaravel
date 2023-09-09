a70704d7623c47f407e417a9add6312876b60e49
Atif Saeed <atifsaeed14@gmail.com>
2023-09-09 08:31:23 +0500

    npm install sweetalert2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+import Swal from 'sweetalert2';
+
+document.getElementById('show-alert').addEventListener('click', () => {
+    Swal.fire({
+        title: 'Hello, Laravel 10!',
+        text: 'Sweetalert2 is now integrated into your Laravel 10 Vite project!',
+        icon: 'success',
+    });
+});
