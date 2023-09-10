9548bce7bd7bb48a971558d48b825a54c7636828
Atif Saeed <atifsaeed14@gmail.com>
2023-09-10 15:07:42 +0500

    add admin theme startbootstrap-sb-admin-2

--- resources/views/agent/dashboard.blade.php
+++ resources/views/agent/dashboard.blade.php
@@ -1,19 +1,17 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Agent Dashboard') }}
+        </h2>
+    </x-slot>
 
-        <title>{{ config('app.name', 'Learn Laravel') }}</title>
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/sass/app.scss', 'resources/js/app.js'])
-
-    </head>
-    <body>
-
-        <h1>Agent dashboard</h1>
-                 
-    </body>
-</html>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
