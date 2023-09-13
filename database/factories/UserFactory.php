921a302ad688f2142f25517e40fd3f1387a5c8d0
Atif Saeed <atifsaeed14@gmail.com>
2023-09-13 10:41:34 +0500

    php artisan migrate:fresh --seed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,8 +24,8 @@ class UserFactory extends Factory
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'phone' => fake()->phoneNumber(),
             'address' => fake()->address(),
-            'photo' => fake()->imageUrl('60', '60'),
-            'role' => fake ()->randomElement (['admin', 'agent', 'user']),
+            'avatar' => fake()->imageUrl('60', '60'),
+            'role' => fake ()->randomElement (['super', 'admin', 'agent', 'user']),
             'status' => fake()->randomElement (['active', 'inactive']),
             'remember_token' => Str::random(10),
         ];
--- database/migrations/2023_09_13_052036_create_user_details_table.php
+++ database/migrations/2023_09_13_052036_create_user_details_table.php
@@ -13,6 +13,30 @@ return new class extends Migration
     {
         Schema::create('user_details', function (Blueprint $table) {
             $table->id();
+            $table->enum('gender', ['male', 'female', 'other'])->default ('other');
+            $table->string('mobile', 100)->nullable();
+            $table->string('lineland', 100)->nullable();
+            $table->date('dob')->nullable();
+
+            $table->string('billing_address1')->nullable();
+            $table->string('billing_address2')->nullable();
+            $table->string('billing_city', 50)->nullable();
+            $table->string('billing_state', 100)->nullable();
+            $table->string('billing_postal_code', 20)->nullable();
+            $table->string('billing_country', 100)->nullable();
+
+            $table->string('shipping_address1')->nullable();
+            $table->string('shipping_address2')->nullable();
+            $table->string('shipping_city', 50)->nullable();
+            $table->string('shipping_state', 100)->nullable();
+            $table->string('shipping_postal_code', 20)->nullable();
+            $table->string('shipping_country', 100)->nullable();
+
+            // $table->unsignedBigInteger('user_id')->index();
+            // $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            // https://www.codechief.org/article/laravel-tips-to-set-foreign-key-in-laravel-migration
+            $table->foreignId('user_id')->index()->constrained()->onDelete('cascade')->onUpdate('cascade');
+
             $table->timestamps();
         });
     }
